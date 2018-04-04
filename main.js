8ce73e9aba1588f98fb8c6090cb710bf4284dc2f
rsbabcock <rachael.s.babcock@gmail.com>
2018-04-04 11:29:59 -0500

    Added chicken monkey code, connected main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+const numbers = Array.from(new Array(101),(x,i) => i);
+// Write a program that prints the numbers from 1 to 100. 
+for(let i = 0; i < numbers.length; i ++) {
+    const currentNumber = numbers[i];
+// But for multiples of five (5, 10, 15, etc.) print "Chicken" instead of the number 
+    if(currentNumber % 5 === 0) {
+        console.log("Chicken");
+// and for the multiples of seven (7, 14, 21, etc.) print "Monkey". 
+    } if (currentNumber % 7 === 0) {
+        console.log("Monkey");
+// For numbers which are multiples of both five and seven print "ChickenMonkey".
+    } if else (currentNumber % 5 === 0 && currentNumber % 7 === 0) {
+        console.log("Chicken Monkey");
+    }
+            
+}
+
